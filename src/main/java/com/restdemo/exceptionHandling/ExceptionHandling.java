7e4a735491805ea848326270e81e3b720ad0ebed
Alexeev Alexandr <fart82@ukr.net>
2019-05-27 13:47:26 +0300

    a

--- src/main/java/com/restdemo/exceptionHandling/ExceptionHandling.java
+++ src/main/java/com/restdemo/exceptionHandling/ExceptionHandling.java
@@ -1,5 +1,9 @@
 package com.restdemo.exceptionHandling;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ExceptionHandling extends RuntimeException {
 
     public ExceptionHandling(String exception){
