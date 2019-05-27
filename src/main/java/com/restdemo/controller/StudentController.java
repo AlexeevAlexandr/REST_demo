0320e016017908be7cc12d552b7a002d1051a3c3
alexandr <fart82@ukr.net>
2019-05-27 21:52:46 +0300

    set db

--- src/main/java/com/restdemo/controller/StudentController.java
+++ src/main/java/com/restdemo/controller/StudentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/student")
+@RequestMapping(value = "/students")
 public class StudentController {
 
     private final StudentRepository studentRepository;
